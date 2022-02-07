f9dc93b65466eced38b10f4936b2a9395d5f6248
zgbsss <99218597+zgbsss@users.noreply.github.com>
2022-02-07 19:17:49 -0300

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,13 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") 
+    
+    {
+    
+    alert(' Muito obrigado por preencher ' + nome.value + ' o envio dos dados foi feito com êxito ');
+    
+    }
+
+}
